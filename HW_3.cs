b35221f67ac86a0b36f97aaa3139ec3909c4875d
qqMarzy <93611432+qqMarzy@users.noreply.github.com>
2021-11-02 20:16:21 +0100

    Add files via upload

---
+++ HW_3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Task3
+    {
+        static void Main(string[] args)
+        {
+            int allPicturesInAlbum = 52;
+            int countOfPicturesInOneLine = 3;
+            int completelyFilledLines;
+            int remainingPictures;
+
+            completelyFilledLines = allPicturesInAlbum / countOfPicturesInOneLine;
+
+            Console.WriteLine("Полностью заполненных рядов - " + completelyFilledLines);
+
+            remainingPictures = allPicturesInAlbum % countOfPicturesInOneLine;
+
+            Console.WriteLine("Оставшиеся картинки - " + remainingPictures);
+
+            Console.ReadKey();
+        }
+    }
+}
