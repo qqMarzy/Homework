29c0aae2bd7fbae53cd889f1acefe2a21c5fdb32
qqMarzy <93611432+qqMarzy@users.noreply.github.com>
2021-11-02 19:20:29 +0100

    Add files via upload

---
+++ HW_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Variables
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            byte age;
+            float weight;
+            bool isMarryed;
+            char personalSymbol;
+            int amountOfMoney;
+            long countOfClients;
+            double percent;
+            int dayCount;
+            ushort amountOfPiggies;
+        }
+    }
+}
