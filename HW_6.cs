79225edd0167e139c4d60b883a97ddcab85d587f
qqMarzy <93611432+qqMarzy@users.noreply.github.com>
2021-11-04 15:14:25 +0100

    Add files via upload

---
+++ HW_6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Task5
+    {
+        static void Main(string[] args)
+        {
+            string name, age, gender, fafavoriteDish;
+
+            Console.Write("Ввведите свое имя: ");
+            name = Console.ReadLine();
+            Console.Write("Ввведите сколько вам лет: ");
+            age = Console.ReadLine();
+            Console.Write("Ввведите ваш пол: ");
+            gender = Console.ReadLine();
+            Console.Write("Ввведите ваше любимое блюдо: ");
+            fafavoriteDish = Console.ReadLine();
+
+            Console.Write($"Вас зовут {name}, вам {age} лет, ваш пол {gender} и ваше любимое блюдо {fafavoriteDish}.");
+
+            Console.ReadKey();
+        }
+    }
+}
