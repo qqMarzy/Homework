209ee4da2a6be87cf3654e18fbe58ea76ed04342
qqMarzy <93611432+qqMarzy@users.noreply.github.com>
2021-11-04 18:30:46 +0100

    Update HW_5.cs

--- HW_5.cs
+++ HW_5.cs
@@ -10,16 +10,20 @@ namespace Homework
     {
         static void Main(string[] args)
         {
-            int peopleInLine;
             const int timeForOnePerson = 10;
+            int peopleInLine;
             int time;
+            int hours;
+            int minutes;
 
             Console.WriteLine("Вы вошли в поликлинику и видите огромную очередь из старушек.\n");
             Console.Write("Сколько старушек вы видите перед собой? - ");
             peopleInLine = Convert.ToInt32(Console.ReadLine());
             time = peopleInLine * timeForOnePerson;
             Console.WriteLine("\nВ поликлинике на одного клиента выделяется ровно " + timeForOnePerson + " минут\n");
-            Console.WriteLine("До вашей очереди осталось - " + time / 60 + " часа и " + time % 60 + " минут");
+            hours = time / 60;
+            minutes = time % 60;
+            Console.WriteLine("До вашей очереди осталось - " + hour + " часа и " + minutes + " минут");
 
             Console.ReadKey();
         }
