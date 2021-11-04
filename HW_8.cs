79622d4aa8808e992058e8ca9c2c3637230c95b0
qqMarzy <93611432+qqMarzy@users.noreply.github.com>
2021-11-04 19:21:03 +0100

    Add files via upload

---
+++ HW_8.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Task8
+    {
+        static void Main(string[] args)
+        {
+            string textForExit;
+
+            while(true)
+            {
+                Console.WriteLine("\nДля завершения цыкла введите \"exit\". \n");
+                textForExit = Console.ReadLine();
+                if (textForExit == "exit")
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Цыкл успешно завершен.");
+
+            Console.ReadKey();
+        }
+    }
+}
