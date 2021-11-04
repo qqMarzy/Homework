09f6a9d6f66a0f84a8325b2eaa3f21f9b94e755a
qqMarzy <93611432+qqMarzy@users.noreply.github.com>
2021-11-04 18:40:50 +0100

    Add files via upload

---
+++ HW_7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Task7
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            int counter;
+
+            Console.Write("Введите свой текст: ");
+            text = Console.ReadLine();
+            Console.Write("\nВведите сколько раз должеж вывестись ваш текст: ");
+            counter = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < counter; i++)
+            {
+                Console.WriteLine(text);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
