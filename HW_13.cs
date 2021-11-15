bb03f2655421edb15f9f4958beb7a310933adcf7
qqMarzy <93611432+qqMarzy@users.noreply.github.com>
2021-11-15 18:27:50 +0100

    Add files via upload

---
+++ HW_13.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Task13
+    {
+        static void Main(string[] args)
+        {
+            const string password = "Anime";
+            string userText = "";
+
+            Console.WriteLine("Введите пароль:");
+
+            for (int i = 0; i < 3 && userText != password; i++)
+            {
+                userText = Console.ReadLine();
+
+                if (userText == password)
+                {
+                    Console.WriteLine("Secret");
+
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Попробуйте еще раз.");
+                }
+            }
+
+        }
+    }
+}
