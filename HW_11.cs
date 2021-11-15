db4fa6f97839995c69ea11623d995776545bf688
qqMarzy <93611432+qqMarzy@users.noreply.github.com>
2021-11-15 17:39:27 +0100

    Add files via upload

---
+++ HW_11.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Task11
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 7; i <= 98; i += 7)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
